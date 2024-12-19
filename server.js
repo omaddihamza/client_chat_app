dd1112116ee3c9d80fc567d5fbf5a663d73a6198
hamza omaddi <hamza.omaddi@gmail.com>
2024-12-19 12:52:14 +0100

    - Installed and set up Socket.IO for real-time communication.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const messages = [
     {name:"tom", message:"hello"},
@@ -15,9 +17,14 @@ app.get("/message", (req, res)=>{
 
 app.post("/message",(req, res)=>{
   messages.push(req.body)
+  io.emit("message",req.body)
     res.sendStatus(200)
 })
 
-const server = app.listen(3000, ()=>{
+io.on('connection',(socket)=>{
+    console.log("a user connected")
+})
+
+const server = http.listen(3000, ()=>{
     console.log('server is listening on port', server.address().port)
 })
